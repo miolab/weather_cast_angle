76caf7bc25943738c5a40b78161f3bac90ab258c
im <poson_hemp@yahoo.co.jp>
2023-11-29 13:36:22 +0900

    env/ deps get httpoison

--- mix.exs
+++ mix.exs
@@ -47,6 +47,7 @@ defmodule WeatherCastAngle.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
+      {:httpoison, "~> 2.2"},
       {:plug_cowboy, "~> 2.5"}
     ]
   end
