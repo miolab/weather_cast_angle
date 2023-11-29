8b33456ac653897ab89bd575bd225c807ecf95ef
im <poson_hemp@yahoo.co.jp>
2023-11-29 13:49:32 +0900

    add/ enable to GET and render sample api response body

--- lib/weather_cast_angle_web/controllers/page_controller.ex
+++ lib/weather_cast_angle_web/controllers/page_controller.ex
@@ -5,13 +5,23 @@ defmodule WeatherCastAngleWeb.PageController do
     # The home page is often custom made,
     # so skip the default app layout.
 
-    # TODO: WIP
-    response = "-- Sample responses --"
+    res =
+      HTTPoison.get("https://www.data.jma.go.jp/gmd/kaiyou/data/db/tide/suisan/txt/2023/MO.txt")
+
+    response_text =
+      case res do
+        {:ok, %HTTPoison.Response{body: response_body}} ->
+          response_body
+
+        {:error, %HTTPoison.Error{reason: reason}} ->
+          # TODO: エラー時にどう振る舞うかをちゃんと書く
+          "Error: " <> to_string(reason)
+      end
 
     render(
       conn,
       :home,
-      response: response,
+      response: response_text,
       layout: false
     )
   end
