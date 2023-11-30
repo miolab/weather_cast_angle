a1a98cb2b60e4f8e11bc200ba86ab5a72f0d326e
im <poson_hemp@yahoo.co.jp>
2023-11-30 09:53:14 +0900

    fix/ separate function get_response_body

--- lib/weather_cast_angle_web/controllers/page_controller.ex
+++ lib/weather_cast_angle_web/controllers/page_controller.ex
@@ -2,21 +2,7 @@ defmodule WeatherCastAngleWeb.PageController do
   use WeatherCastAngleWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-
-    res =
-      HTTPoison.get("https://www.data.jma.go.jp/gmd/kaiyou/data/db/tide/suisan/txt/2023/MO.txt")
-
-    response_text =
-      case res do
-        {:ok, %HTTPoison.Response{body: response_body}} ->
-          response_body
-
-        {:error, %HTTPoison.Error{reason: reason}} ->
-          # TODO: エラー時にどう振る舞うかをちゃんと書く
-          "Error: " <> to_string(reason)
-      end
+    response_text = WeatherCastAngle.Services.ResponseProcessor.get_response_body(2023, "MO")
 
     render(
       conn,
