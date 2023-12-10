6d7807ee33df056927f880c6612b7cd84af928d6
im <poson_hemp@yahoo.co.jp>
2023-12-10 18:56:27 +0900

    add/ chart.js sample render
    ref: https://www.chartjs.org/docs/latest/getting-started/

---
+++ assets/js/lib/chartRender.js
@@ -0,0 +1,26 @@
+import Chart from "chart.js/auto";
+
+export function renderChart() {
+  const ctx = document.getElementById("chartArea");
+
+  new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+}
