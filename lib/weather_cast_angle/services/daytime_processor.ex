7fc8d0a2cd7e489bc9887043155742e7f015ba14
im <poson_hemp@yahoo.co.jp>
2023-12-15 08:12:58 +0900

    chore/ add test of get_current_date_string using Timex stub

--- lib/weather_cast_angle/services/daytime_processor.ex
+++ lib/weather_cast_angle/services/daytime_processor.ex
@@ -1,4 +1,8 @@
 defmodule WeatherCastAngle.Services.DaytimeProcessor do
+  @doc """
+  Get current date and return the YYYY-MM-DD formatted date string.
+  """
+  @spec get_current_date_string() :: String.t()
   def get_current_date_string() do
     Timex.format!(get_current_date(), "{YYYY}-{0M}-{0D}")
   end
