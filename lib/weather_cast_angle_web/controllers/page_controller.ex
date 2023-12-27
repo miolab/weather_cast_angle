25e51f59ba483645199ee15596494a62ee1b1673
im <poson_hemp@yahoo.co.jp>
2023-12-27 18:32:20 +0900

    chore/ specify response -> tide_response

--- lib/weather_cast_angle_web/controllers/page_controller.ex
+++ lib/weather_cast_angle_web/controllers/page_controller.ex
@@ -7,12 +7,12 @@ defmodule WeatherCastAngleWeb.PageController do
     current_date = WeatherCastAngle.Services.DatetimeProcessor.get_current_date_string()
     default_tide_location_code = WeatherCastAngle.Utils.Locations.default_tide_location_code()
 
-    response_map = fetch_response_map(current_date, default_tide_location_code)
+    tide_response_map = fetch_tide_response_map(current_date, default_tide_location_code)
 
     render(
       conn,
       :home,
-      response: response_map[current_date],
+      tide_response: tide_response_map[current_date],
       selected_location: WeatherCastAngle.Utils.Locations.default_location_name(),
       location_names: @location_names,
       layout: false
@@ -24,19 +24,19 @@ defmodule WeatherCastAngleWeb.PageController do
         "location_name" => location_name
       }) do
     tide_location_code = WeatherCastAngle.Utils.Locations.get_location_code_by_name(location_name)
-    response_map = fetch_response_map(input_date, tide_location_code)
+    tide_response_map = fetch_tide_response_map(input_date, tide_location_code)
 
     render(
       conn,
       :home,
-      response: response_map[input_date],
+      tide_response: tide_response_map[input_date],
       selected_location: location_name,
       location_names: @location_names,
       layout: false
     )
   end
 
-  defp fetch_response_map(date_string, location_code) do
+  defp fetch_tide_response_map(date_string, location_code) do
     [year | _] = String.split(date_string, "-")
 
     WeatherCastAngle.Services.TideDataHandler.get_response_body(
