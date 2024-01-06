772aff4b34ae266ef199e44a8c00d48f60f3ccaa
im <poson_hemp@yahoo.co.jp>
2024-01-07 08:56:24 +0900

    add/ extract_weather_forecast/1

--- lib/weather_cast_angle/services/weather_forecast_handler.ex
+++ lib/weather_cast_angle/services/weather_forecast_handler.ex
@@ -14,4 +14,31 @@ defmodule WeatherCastAngle.Services.WeatherForecastHandler do
       @forecast_url
     )
   end
+
+  @doc """
+  Extracts and transforms specific weather forecast.
+  """
+  @spec extract_weather_forecast(String.t()) :: map()
+  def extract_weather_forecast(location_name) do
+    forecast_maps = get_weather_forecast(location_name)["list"]
+
+    Enum.reduce(forecast_maps, %{}, fn forecast_map, acc ->
+      weather_map = Enum.at(forecast_map["weather"], 0, %{})
+
+      Map.put(
+        acc,
+        forecast_map["dt"]
+        |> WeatherCastAngle.Services.DatetimeProcessor.convert_unix_to_datetime_string(),
+        %{
+          weather_description: weather_map |> Map.get("description", ""),
+          weather_main: weather_map |> Map.get("main", ""),
+          wind_speed: forecast_map["wind"]["speed"],
+          main_temp:
+            forecast_map["main"]["temp"]
+            |> WeatherCastAngle.Services.WeatherDataProcessor.kelvin_to_celsius_temperature(),
+          main_humidity: forecast_map["main"]["humidity"]
+        }
+      )
+    end)
+  end
 end
