dbb9cdbe22fc38099f7d639e3ca797af088bfec0
im <poson_hemp@yahoo.co.jp>
2024-01-21 11:04:58 +0900

    prepare/ function get_previous_days_temperatures

--- lib/weather_cast_angle/services/sea_water_temperature_handler.ex
+++ lib/weather_cast_angle/services/sea_water_temperature_handler.ex
@@ -1,2 +1,22 @@
 defmodule WeatherCastAngle.Services.SeaWaterTemperatureHandler do
+  # TODO: 各areaコードをlocationに追加する
+  @sea_area_code 602
+
+  def get_previous_days_temperatures() do
+    url =
+      "https://www.data.jma.go.jp/kaiyou/data/db/kaikyo/series/engan/txt/area#{@sea_area_code}.txt"
+
+    res = HTTPoison.get(url)
+
+    case res do
+      {:ok, %HTTPoison.Response{body: response_body}} ->
+        result =
+          response_body
+
+        result
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        %{"Error" => reason}
+    end
+  end
 end
