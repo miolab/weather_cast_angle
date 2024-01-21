6616832a9d28d45a4efa8eabde72f4a01445641c
im <poson_hemp@yahoo.co.jp>
2024-01-21 17:43:42 +0900

    fix/ enable to cache response body

--- lib/weather_cast_angle/services/sea_water_temperature_handler.ex
+++ lib/weather_cast_angle/services/sea_water_temperature_handler.ex
@@ -1,22 +1,38 @@
 defmodule WeatherCastAngle.Services.SeaWaterTemperatureHandler do
   # TODO: 各areaコードをlocationに追加する
-  @sea_area_code 602
+  # @sea_area_code 602
 
-  def get_previous_days_temperatures() do
-    url =
-      "https://www.data.jma.go.jp/kaiyou/data/db/kaikyo/series/engan/txt/area#{@sea_area_code}.txt"
+  def get_previous_days_temperatures(sea_area_code) do
+    cache_key = "#{sea_area_code}_area_previous_days_temperatures"
 
-    res = HTTPoison.get(url)
+    _get_previous_days_temperatures_data(sea_area_code, cache_key)
+  end
 
-    case res do
-      {:ok, %HTTPoison.Response{body: response_body}} ->
-        result =
-          response_body
+  defp _get_previous_days_temperatures_data(sea_area_code, cache_key) do
+    cached_value = WeatherCastAngle.Cache.get_cache(cache_key)
 
-        result
+    case cached_value do
+      nil ->
+        url =
+          "https://www.data.jma.go.jp/kaiyou/data/db/kaikyo/series/engan/txt/area#{sea_area_code}.txt"
 
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        %{"Error" => reason}
+        res = HTTPoison.get(url)
+
+        case res do
+          {:ok, %HTTPoison.Response{body: response_body}} ->
+            result =
+              response_body
+
+            WeatherCastAngle.Cache.put_cache(cache_key, result, 60)
+
+            result
+
+          {:error, %HTTPoison.Error{reason: reason}} ->
+            %{"Error" => reason}
+        end
+
+      _ ->
+        cached_value
     end
   end
 end
