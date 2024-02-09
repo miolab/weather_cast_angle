7b0b8e87d72c9a21dfd1ab8d191f452878cfc49a
im <poson_hemp@yahoo.co.jp>
2024-02-10 08:13:27 +0900

    fix/ add and apply validate_lowercase_alphabetic_length/3 to input location_name

--- lib/weather_cast_angle/utils/validation.ex
+++ lib/weather_cast_angle/utils/validation.ex
@@ -13,4 +13,35 @@ defmodule WeatherCastAngle.Utils.Validation do
       false -> {:error, "Invalid date format"}
     end
   end
+
+  @doc """
+  Validates that the given value consists of lowercase alphanumeric characters and is within a specific range of characters long.
+  """
+  @spec validate_lowercase_alphabetic_length(
+          String.t(),
+          pos_integer(),
+          pos_integer()
+        ) :: :ok | {:error, String.t()}
+  def validate_lowercase_alphabetic_length(value, min, max)
+      when 1 <= min and min <= max and is_integer(min) and is_integer(max) do
+    cond do
+      String.length(value) < min ->
+        {:error, "The value must be at least #{min} characters long."}
+
+      String.length(value) > max ->
+        {:error, "The value must be no more than #{max} characters long."}
+
+      Regex.match?(~r/^[a-z]+$/, value) ->
+        :ok
+
+      true ->
+        {:error, "The value must consist of lowercase alphabetic characters only."}
+    end
+  end
+
+  def validate_lowercase_alphabetic_length(_, _, _),
+    do: {
+      :error,
+      "Invalid parameters: min must be at least 1 and max must be equal to or greater integer than min."
+    }
 end
