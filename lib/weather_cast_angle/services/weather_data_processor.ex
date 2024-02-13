296e9565dec97df99cbf0defbb143ccd921d72c2
im <poson_hemp@yahoo.co.jp>
2024-02-14 08:18:43 +0900

    refactor/ apply alias to WeatherDataProcessor

--- lib/weather_cast_angle/services/weather_data_processor.ex
+++ lib/weather_cast_angle/services/weather_data_processor.ex
@@ -1,11 +1,17 @@
 defmodule WeatherCastAngle.Services.WeatherDataProcessor do
+  @moduledoc """
+  Provides various functions for processing weather data.
+  """
+  alias WeatherCastAngle.Utils
+  alias WeatherCastAngle.Cache
+
   @open_weather_api_key System.get_env("OPEN_WEATHER_API_KEY")
 
   @spec get_open_weather_data(String.t(), String.t(), String.t()) :: map()
   def get_open_weather_data(location_name, cache_key, target_url) do
     # TODO: location_name が location_names に含まれるか保証するため assertion 入れる。含まれなかったら default_name の情報を返して、default のレンダリングをするのでよい。リダイレクトする方針も検討
 
-    cached_value = WeatherCastAngle.Cache.get_cache(cache_key)
+    cached_value = Cache.get_cache(cache_key)
 
     case cached_value do
       nil ->
@@ -16,7 +22,7 @@ defmodule WeatherCastAngle.Services.WeatherDataProcessor do
 
         case res do
           {:ok, %HTTPoison.Response{body: response_body}} ->
-            WeatherCastAngle.Cache.put_cache(cache_key, response_body, 5)
+            Cache.put_cache(cache_key, response_body, 5)
 
             response_body |> Jason.decode!()
 
@@ -30,7 +36,7 @@ defmodule WeatherCastAngle.Services.WeatherDataProcessor do
   end
 
   defp _query_parameters_for_get_weather_data(location_name) do
-    location_map = WeatherCastAngle.Utils.Locations.get_location_map_by_name(location_name)
+    location_map = Utils.Locations.get_location_map_by_name(location_name)
 
     %{
       lat: location_map |> Map.get(:latitude) |> Float.to_string(),
