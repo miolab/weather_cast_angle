dda635db3220cc977d765e796fdc128637c14038
im <poson_hemp@yahoo.co.jp>
2025-01-03 08:57:27 +0900

    prepare/ defmodule Services.Forecast

---
+++ lib/weather_cast_angle/services/forecast.ex
@@ -0,0 +1,5 @@
+defmodule WeatherCastAngle.Services.Forecast do
+  @moduledoc """
+  Provides functions about forecasts of weather, wind speed, etc. for the day and for the next several days.
+  """
+end
