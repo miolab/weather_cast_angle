6f8617ddf8493258fd0c91ab7ae66bb301961deb
im <poson_hemp@yahoo.co.jp>
2025-02-10 08:27:15 +0900

    fix/ enable to handle multiple js-format-japanese-date elements

--- assets/js/lib/dateFormatter.ts
+++ assets/js/lib/dateFormatter.ts
@@ -9,7 +9,22 @@ dayjs.locale("ja");
  * @param dateString - “YYYYY-MM-DD” formatted Date string.
  * @returns {string} - "1月23日(土)" formatted Date string in Japanese.
  */
-export function formatDate(dateString: string): string {
+const formatDate = (dateString: string): string => {
   const date = dayjs(dateString);
   return date.format("M月D日(ddd)");
-}
+};
+
+/**
+ * Finds all elements with the class `.js-format-japanese-date`,
+ * and formats their `data-date` attribute into Japanese date format.
+ */
+export const autoFormatJapaneseDates = (): void => {
+  document
+    .querySelectorAll<HTMLElement>(".js-format-japanese-date")
+    .forEach((element) => {
+      const rawDate = element.getAttribute("data-date");
+      if (rawDate) {
+        element.textContent = formatDate(rawDate);
+      }
+    });
+};
